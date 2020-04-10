2d8e1335a62161e1593168798d3fba8b2ce4ea01
Alvis HT Tang <alvis@hilbert.space>
2020-04-10 19:53:11 +0100

    exp(devops): add `kns` for kubernetes context switchiing

--- setups/devops.sh
+++ setups/devops.sh
@@ -19,11 +19,16 @@
 #  || awscli: CLI for handling AWS resources
 #  || google-cloud-sdk: CLI for handling GCS resources
 #  || docker: docker for MacOS
+#  || kns: for switching K8S namespaces and context [ EXPERIMENTAL ]
+#  || > usage
+#  ||   - kns: switch namespace
+#  ||   - ktx: switch context
 #  || kubernetes-cli: CLI for handling K8S resources
 # //
 
 brew install\
   awscli\
+  blendle/blendle/kns
   kubernetes-cli\
   && true
 
