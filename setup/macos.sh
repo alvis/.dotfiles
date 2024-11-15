e53aa09fdf61fdd7c6146be452ae3275bd3daafd
Alvis HT Tang <alvis@hilbert.space>
2024-11-15 13:02:13 +0000

    feat(setup): enter password once and for all

--- setup/macos.sh
+++ setup/macos.sh
@@ -15,6 +15,20 @@
 #
 
 BASE=$(cd "$(dirname "$0")"; pwd -P)
+# temporarily disable the need for typing the sudo password multiple times in the current shell session after it is initially verified
+
+ME=$(whoami)
+UUID=$(head /dev/urandom | LC_ALL=C tr -dc 'a-z0-9' | head -c 8)
+SUDO_FILE="/etc/sudoers.d/$UUID"
+sudo -S -k -- bash -c "{
+  echo '$ME ALL=(ALL) NOPASSWD: ALL';
+} | sudo tee -a '$SUDO_FILE' >/dev/null"
+
+# explanation:
+# -S: allows sudo to read the password from standard input, enabling interactive password entry
+# -k: forces sudo to invalidate the current timestamp, requiring a password for the next sudo command
+# bash -c: runs a command string to append the temporary configuration to a sudoers file
+# /etc/sudoers.d/$UUID: creates a unique temporary file in the sudoers.d directory to avoid modifying the main sudoers file
 
 # set for automatic installation
 export NONINTERACTIVE=1
@@ -44,3 +58,7 @@ $BASE/macos/application.macos.sh
 # setup .zshrc
 
 ln -sf $BASE/../.common.zshrc ~/.common.zshrc
+
+# finish the sudo session
+sudo rm $SUDO_FILE
+sudo -k
